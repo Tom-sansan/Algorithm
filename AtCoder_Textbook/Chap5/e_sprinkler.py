cd3b3c3eadd7069a1af1b2ad763bc01e145cc31e
Tomoyasu <tms.iwasawa@gmail.com>
2022-03-21 17:58:49 +0900

    Added E - Sprinkler

--- AtCoder_Textbook/Chap5/e_sprinkler.py
+++ AtCoder_Textbook/Chap5/e_sprinkler.py
@@ -4,6 +4,7 @@
 ########################################
 # 隣接行列
 ########################################
+'''
 N, M, Q = map(int, input().split())
 # False N x N の2次元配列
 graph = []
@@ -45,8 +46,8 @@ for i in range(0, Q):
         # 全ての頂点を順番に見る
         for i in range(0, N):
 
-            # 頂点i が頂点 x に隣接している場合、
-            # すなわは頂点 x と頂点 i の間に辺がある場合
+            # 頂点 i が頂点 x に隣接している場合、
+            # すなわち頂点 x と頂点 i の間に辺がある場合
             if graph[x][i]:
 
                 # 頂点 i の色を C[x] に書き換える
@@ -65,3 +66,66 @@ for i in range(0, Q):
 
         # 頂点 x の色を y に書き換える
         C[x] = y
+'''
+########################################
+# 隣接リスト
+########################################
+N, M, Q = map(int, input().split())
+
+# N x 0 の2次元配列
+graph = []
+
+for i in range(0, N):
+    # 長さ 0 の1次元配列
+    row = []
+    graph.append(row)
+
+# M 本の辺を受け取る
+for i in range(0, M):
+    u, v = map(int, input().split())
+
+    # 頂点番号は全て-1
+    u -= 1
+    v -= 1
+
+    # 頂点 u から v へ辺がある
+    graph[u].append(v)
+
+    # 頂点 v から U へ辺がある
+    graph[v].append(u)
+
+# 頂点の色のリストを受け取る
+C = list(map(int, input().split()))
+
+# Q 個のクエリを受け取る
+for i in range(0, Q):
+    query = list(map(int, input().split()))
+
+    # スプリンクラーを起動するクエリ
+    if query[0] == 1:
+        x = query[1]
+
+        # 頂点番号を全て-1
+        x -= 1
+
+        # 頂点 x の色を出力
+        print(C[x])
+
+        # 頂点 x に隣接する頂点の色を頂点 x と同じ色にする
+        for i in graph[x]:
+            C[i] = C[x]
+
+    # スプリンクラーを起動しないクエリ
+    if query[0] == 2:
+        x = query[1]
+        y = query[2]
+        
+        # 頂点番号は全て-1
+        x -= 1
+
+        # 頂点 x の色を出力する
+        print(C[x])
+
+        # 頂点 x の色を y に書き換え
+        C[x] = y
+########################################
